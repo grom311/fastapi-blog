1b39a2d591dcf1d50db026d0d550d13ac97258aa
grom311 <grom311@yandex.ru>
2022-07-29 13:05:36 +0300

    test

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,7 +11,6 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-
 @router.post('/login')
 def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.name == request.username).first()
